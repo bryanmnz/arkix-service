949c22a038fa4822ec0f4b638266bc21ddbf8c39
Bryan Munoz <bryan.munoz.ext@claro.com.co>
2019-09-18 16:46:24 -0500

    Agrega configuracion base de datos

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || '3000',
     NODE: process.env.NODE_ENV || 'dev',
-    CONN_STR: (process.env.NODE_ENV =='dev')?'mongodb://localhost:27017/cafe':'mongodb+srv://arkix:<password>@cluster0-bovde.mongodb.net/test?retryWrites=true&w=majority'
+    CONN_STR: (process.env.NODE_ENV =='dev')?'mongodb://localhost:27017/cafe':'mongodb+srv://arkix:Colombia20151@cluster0-bovde.mongodb.net/test?retryWrites=true&w=majority/cafe'
 }
